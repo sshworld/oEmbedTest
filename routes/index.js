d9f859c98fa8b28c6fae7581bda409e296354a9f
sshworld <ssh97540@gmail.com>
2022-01-11 20:37:32 +0900

    Create Search url to oEmbedUrl

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,46 @@ var express = require('express');
 var router = express.Router();
 const url = require('url')
 const oembed = require('oembed-auto')
-const provider = require('../provider.json')
+const provider = require('../provider.json');
+const request = require('request')
+
+let providerUrl = []
+for(var a of provider) {
+  providerUrl.push(a.endpoints[0].url)
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(provider)
   res.render('index', {pages : 'main/main'});
 });
 
 /* GET Search Data  */
-router.post('/', function(req, res) {
-  // console.log(req.body)
-  // const urlStr = req.body.url;
+router.get('/search', function(req, res) {
 
-  // const urlObj = url.parse(urlStr, true);
+  const urlStr = url.parse(req.query.url, true);
+  let str = ''
+  let oembedUrl = ''
+  let hostname = urlStr.hostname.split('.')
+  hostname = hostname[hostname.length - 2]
+  for(var host of providerUrl) {
+    if(host.includes(hostname)) {
+      str = host;
+      break;
+    }
+  }
 
-  // console.log(urlObj.hostname + '/oembed?url=' + urlObj.href + '&format=json')
-
-  console.log(req.body)
+  if (str.includes("oembed.")) {
+    str = str.replace("{format}", "json");
+    oembedUrl = str + "?url=" + urlStr.href
+  } else {
+    oembedUrl = str + '?format=json&url=' + urlStr.href
+  }
   
-
-  res.render('index', {pages : 'main/main', oEmbedInfo : 'dd'})
+  request(oembedUrl, (err, response, body) => {
+    var json = JSON.parse(body)
+    res.send(json)
+  })
+  
 });
 
 module.exports = router;
